3d8a139d406c9fc86f92d38b0e38e5879e45784a
Micah Thomas <thoma223@wwu.edu>
2025-01-26 16:02:35 -0800

    Added the rest of the methods in SurlyDatabase, Relation, and Tuple, as well as added indepth comments for everything

--- Surly1/Tuple.java
+++ Surly1/Tuple.java
@@ -1,6 +1,25 @@
+import java.util.Arrays;
+import java.util.LinkedList;
 public class Tuple {
     private LinkedList<AttributeValue> values;
-    public String getValue(String attributeName){
 
+    /*This is a constructor for a tuple which just takes in a list of Attributevalues, and sets them to the
+     * values list.
+     */
+    public Tuple(String[] values){
+        this.values=new LinkedList<>(Arrays.asList(values));
+    }
+
+    /*This just gets the value of a specific attribute by searching for the attribute name, and then
+     * return the specific attribute
+    */
+    public String getValue(String attributeName){
+        for(AttributeValue value:values){
+            if(value.getName().equals(attributeName)){
+                return value.getValue();
+            }
+        }
+        System.out.println("Value not found");
+        return null;
     }
 }
